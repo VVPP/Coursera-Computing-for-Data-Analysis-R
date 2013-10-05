dd4496e452414d2f56224c87e6a7b628c03bae0f
4n4kin <nileshsubscriptions@gmail.com>
2013-10-06 03:09:15 +0530

    Edited

--- Week 1/corr.R
+++ Week 1/corr.R
@@ -2,6 +2,7 @@
 W#rite a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0. A prototype of this function follows
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
+  
   ## the location of the CSV files
   
   ## 'threshold' is a numeric vector of length 1 indicating the
--- Week 1/getmonitor.R
+++ Week 1/getmonitor.R
@@ -1,21 +1,18 @@
-#Write a function named 'getmonitor' that takes three arguments: 'id', 'directory', and 'summarize'. Given a monitor ID number, 'getmonitor' reads that monitor's particulate matter data from the directory specified in the 'directory' argument and returns a data frame containing that monitor's data. If 'summarize = TRUE', then 'getmonitor' produces a summary of the data frame with the 'summary' function and prints it to the console. A prototype of the function is as follows
-getmonitor <- function(id, directory, summarize = FALSE) {
+getmonitor <- function(id, directory, summarize = FALSE){
+  
   ## 'id' is a vector of length 1 indicating the monitor ID
-  id=as.numeric(id)
   ## number. The user can specify 'id' as either an integer, a
-  
-  ## character, or a numeric.
-  
-  ## 'directory' is a character vector of length 1 indicating
-  
+  ## character
+  ## 'directory' is a character vector of length 1 indicating, or a numeric.
   ## the location of the CSV files
-  
   ## 'summarize' is a logical indicating whether a summary of
   ## the data should be printed to the console; the default is
   ## FALSE
-  
-  ## Your code here
+  id <- as.numeric(id)
+  filename <- paste(sprintf("%03.f",id), "csv", sep = ".")
+  path <- paste(directory, filename, sep = "/")
+  content <- read.csv(path)
+  if(summarize == TRUE){
+    print(summary(content))
+  }
 }
-#You can see some example output from this function. The function that you write should be able to match this output. Please save your code to a file named getmonitor.R. To run the test script for this part, make sure your working directory has the file getmonitor.R in it and the run
-source("http://spark-public.s3.amazonaws.com/compdata/scripts/getmonitor-test.R")
-getmonitor.testscript()
