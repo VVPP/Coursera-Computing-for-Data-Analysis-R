d74249cd2e1ad2911551c6260f38f5566efa6449
4n4kin <nileshsubscriptions@gmail.com>
2013-10-07 10:28:35 +0530

    Week 1 Completed

--- Week 1/corr.R
+++ Week 1/corr.R
@@ -1,8 +1,5 @@
-#Part 3
-W#rite a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0. A prototype of this function follows
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
-  
   ## the location of the CSV files
   
   ## 'threshold' is a numeric vector of length 1 indicating the
@@ -11,9 +8,15 @@ corr <- function(directory, threshold = 0) {
   ## nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
+  nFiles<-length(list.files(directory))
+  vec<-c()
+  
+  monitorsToCorr<-subset(complete(directory,1:nFiles),nobs>threshold)[1][,1]
+  formatMonitor <-function(i,directory) subset(getmonitor(i,directory),sulfate!="NA" & nitrate!="NA",select=c(sulfate,nitrate))
+  
+  for(i in monitorsToCorr){
+    monitor<-formatMonitor(i,directory)
+    vec<-c(vec,cor(monitor)[2])
+  }
+  return(vec)
 }
-For this function you will need to use the 'cor' function in R which calculates the correlation between two vectors. Please read the help page for this function via '?cor' and make sure that you know how to use it.
-You can see some example output from this function. The function that you write should be able to match this output. Please save your code to a file named corr.R. To run the test script for this part, make sure your working directory has the file corr.R in it and the run
-
-source("http://spark-public.s3.amazonaws.com/compdata/scripts/corr-test.R")
-corr.testscript()
--- Week 1/getmonitor.R
+++ Week 1/getmonitor.R
@@ -1,18 +1,24 @@
-getmonitor <- function(id, directory, summarize = FALSE){
-  
+getmonitor <- function(id,directory,summarize=FALSE){
   ## 'id' is a vector of length 1 indicating the monitor ID
   ## number. The user can specify 'id' as either an integer, a
-  ## character
-  ## 'directory' is a character vector of length 1 indicating, or a numeric.
+  ## character, or a numeric.
+  
+  ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
+  
   ## 'summarize' is a logical indicating whether a summary of
   ## the data should be printed to the console; the default is
   ## FALSE
-  id <- as.numeric(id)
-  filename <- paste(sprintf("%03.f",id), "csv", sep = ".")
-  path <- paste(directory, filename, sep = "/")
-  content <- read.csv(path)
-  if(summarize == TRUE){
-    print(summary(content))
-  }
+  file_type<-".csv"
+  # pad with zeros 
+  padz <- function(x, n=max(nchar(x))) gsub(" ", "0", formatC(x, width=n)) 
+  
+  file_path=gsub(" ","",paste(directory,"/",padz(id,3),file_type,sep=""))
+  data<-read.csv(file_path)
+  
+  if (summarize==TRUE)
+    print (summary(data))    
+  
+  return(data)
+  
 }
